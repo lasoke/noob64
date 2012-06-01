c20c793e19b53bef3e5a0ae3ce8342a52eb54e67
PtiPingouin <ptipingouin@gmail.com>
2012-06-01 19:03:17 +0200

    (Forgot to add the updated Main function to the last commit)

--- Noob64/Noob64.cpp
+++ Noob64/Noob64.cpp
@@ -19,9 +19,23 @@ int main(array<System::String ^> ^args)
 {
 	enableConsole();
 
-	RDRAM*	ram = new RDRAM();
-	R4300i* cpu = new R4300i();
-	cpu->start();
+	RDRAM*		ram = new RDRAM();
+	R4300i*		cpu = new R4300i(*ram);
+	RomLoader*	rom = new RomLoader();
+
+	char		addr[8];
+	int			instr_s = sizeof(word);
+	for (int i = 0; i < BOOT_CODE_SIZE; i++)
+	{
+		if (DEBUG)
+		{
+			memset(addr, 0, 8);
+			itoa(0x40 + i * instr_s, addr, 16);
+			cout << endl << format_number(string(8 - strlen(addr), '0') + addr, ' ', 2) << " : ";
+		}
+		cpu->decode(rom->getInstruction(i));
+	}
+
 	/*
 	ram->write<word>(4294967295, 1);
 	ram->write<word>(0, 2);
