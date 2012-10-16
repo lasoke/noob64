03afd799abe0ede66b05ab7b84073969c9d8ef7e
PtiPingouin <ptipingouin@gmail.com>
2012-10-16 19:10:26 +0200

    now using [] in rsp.cpp

--- Noob64/R4300i.cpp
+++ Noob64/R4300i.cpp
@@ -999,7 +999,7 @@ void R4300i::LW(int rt, int immed, int rs)
 #	if defined DEBUG
 		cout << print_addr() << "LW " << dec << "r" << rt << " 0x" << hex << extend_sign_halfword(immed) << "[" << dec << "r" << rs << "]";
 #	endif // DEBUG
-	r[rt] = memory->read<sword>(r[rs] + extend_sign_halfword(immed));
+	r[rt] = extend_sign_word(memory->read<sword>(r[rs] + extend_sign_halfword(immed)));
 #	if defined DEBUG
 		cout << " r" << dec << rt << hex << "=0x" << r[rt];
 #	endif // DEBUG
