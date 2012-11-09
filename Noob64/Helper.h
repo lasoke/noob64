0f28a43665cb9f010a05d775c667dc06c8279f9c
PtiPingouin <ptipingouin@gmail.com>
2012-11-09 19:03:37 +0100

    -moved every inline or template method to a .hxx file -cleaned up some useless fields and includes

--- Noob64/Helper.h
+++ Noob64/Helper.h
@@ -33,35 +33,11 @@ string print_addr(word address);
 // Returns the word contained at the address pointed by ptr
 byte getByte(char **ptr);
 
-//print the memory contains in the array data
+// Prints the memory contained in the array data
 void dump_array(word start_addr, const byte *data, unsigned int size, int type);
 
-//****************************************************************************
-//** CONVERTS A BYTE ARRAY TO ITS Type REPRESENTATION						**
-//****************************************************************************
-template <typename Type>
-inline Type binary_to_type(const byte *address)
-{
-	Type result = 0;
-	for (unsigned int i = 0; i < sizeof(result); i++)
-		result = (result << 8) + address[i];
-	return result;
-}
+// Converts a byte array to its type representation
+template <typename Type> inline Type binary_to_type(const byte *address);
 
-//****************************************************************************
-//** CONVERTS A Type TO ITS BINARY REPRESENTATION							**
-//****************************************************************************
-template <typename Type>
-inline Type type_to_binary(Type data)
-{
-	byte *tab = (byte*) &data;
-	byte b;
-	unsigned size = sizeof(data);
-	for (unsigned int i = 0; i < size / 2; i++)
-	{
-		b = tab[i];
-		tab[i] = tab[size-1-i];
-		tab[size-1-i] = b;
-	}
-	return data;
-}
+// Converts a type to its binary representation
+template <typename Type> inline Type type_to_binary(Type data);
