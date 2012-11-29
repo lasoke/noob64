c2c197922ef6ccfd1cfc5ff92032220556c56343
PtiPingouin <ptipingouin@gmail.com>
2012-11-29 21:22:09 +0100

    added TimerHandler.hxx

---
+++ Noob64/TimerHandler.hxx
@@ -0,0 +1,31 @@
+/*
+ * Noob64 - A Nintendo 64 emulator.
+ *
+ * (c) Copyright 2012 Quentin Metzler and 
+ * Romain Richard.
+ *
+ * Permission to use, copy, modify and distribute Noob64 in both binary and
+ * source form, for non-commercial purposes, is hereby granted without fee,
+ * providing that this license information and copyright notice appear with
+ * all copies and any derived work.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event shall the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Noob64 is freeware for PERSONAL USE only. Commercial users should
+ * seek permission of the copyright holders first. Commercial use includes
+ * charging money for Noob64 or software derived from Noob64.
+ *
+ * The copyright holders request that bug fixes and improvements to the code
+ * should be forwarded to them so if they want them.
+ *
+ */
+
+#pragma once
+
+#include "TimerHandler.h"
+
+inline int TimerHandler::getTimer(void)			{ return timer; }
+inline void TimerHandler::setTimer(int t)		{ timer = t; }
+inline int TimerHandler::getNextTimer(int type)	{ return next_timer[type]; }
